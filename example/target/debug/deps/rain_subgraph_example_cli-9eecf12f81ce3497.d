d15937f26695fbaee742b022edbd44445d158089
thedavidmeister <thedavidmeister@gmail.com>
2024-01-08 17:51:41 +0400

    fix example rs test

---
+++ example/target/debug/deps/rain_subgraph_example_cli-9eecf12f81ce3497.d
@@ -0,0 +1,5 @@
+/Users/thedavidmeister/Code/rain.subgraph.docker/example/target/debug/deps/rain_subgraph_example_cli-9eecf12f81ce3497: crates/cli/src/main.rs
+
+/Users/thedavidmeister/Code/rain.subgraph.docker/example/target/debug/deps/rain_subgraph_example_cli-9eecf12f81ce3497.d: crates/cli/src/main.rs
+
+crates/cli/src/main.rs:
