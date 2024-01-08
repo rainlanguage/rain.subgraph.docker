b5c2e9c13acc90d435f4f1bbbcd8734948f93b35
thedavidmeister <thedavidmeister@gmail.com>
2024-01-08 17:52:17 +0400

    remove target

--- example/target/debug/deps/rain_subgraph_example_cli-9eecf12f81ce3497.d
+++
@@ -1,5 +0,0 @@
-/Users/thedavidmeister/Code/rain.subgraph.docker/example/target/debug/deps/rain_subgraph_example_cli-9eecf12f81ce3497: crates/cli/src/main.rs
-
-/Users/thedavidmeister/Code/rain.subgraph.docker/example/target/debug/deps/rain_subgraph_example_cli-9eecf12f81ce3497.d: crates/cli/src/main.rs
-
-crates/cli/src/main.rs:
